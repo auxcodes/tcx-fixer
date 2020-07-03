cf577b065cf8a6f47de90c31c97b7409e606aea4
Borgosity <borgosity@gmail.com>
2020-07-04 07:43:58 +1000

    add basic layout for gui

--- TcxFixerGui/MainWindow.Designer.cs
+++ TcxFixerGui/MainWindow.Designer.cs
@@ -29,26 +29,75 @@
         private void InitializeComponent()
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(12, 13);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(94, 15);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(694, 20);
+            this.pathTextBox.TabIndex = 1;
+            this.pathTextBox.Text = "File/Folder...";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(713, 42);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(13, 73);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(775, 365);
+            this.outputTextBox.TabIndex = 3;
+            this.outputTextBox.Text = "Output...";
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.browseButton);
             this.Name = "MainWindow";
             this.Text = "Tcx Fixer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox outputTextBox;
     }
 }
 
