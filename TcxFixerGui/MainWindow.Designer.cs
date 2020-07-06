8f95cf2da1db7f43441ec5c26990a0ced1fb14a1
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2020-07-06 23:34:55 +1000

    Test file dialogs

--- TcxFixerGui/MainWindow.Designer.cs
+++ TcxFixerGui/MainWindow.Designer.cs
@@ -29,61 +29,89 @@
         private void InitializeComponent()
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.browseButton = new System.Windows.Forms.Button();
+            this.selectFileButton = new System.Windows.Forms.Button();
             this.pathTextBox = new System.Windows.Forms.TextBox();
             this.startButton = new System.Windows.Forms.Button();
             this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.SuspendLayout();
             // 
             // openFileDialog1
             // 
+            this.openFileDialog1.DefaultExt = "tcx";
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // browseButton
+            // selectFileButton
             // 
-            this.browseButton.Location = new System.Drawing.Point(12, 13);
-            this.browseButton.Name = "browseButton";
-            this.browseButton.Size = new System.Drawing.Size(75, 23);
-            this.browseButton.TabIndex = 0;
-            this.browseButton.Text = "Browse";
-            this.browseButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.AutoSize = true;
+            this.selectFileButton.Location = new System.Drawing.Point(18, 20);
+            this.selectFileButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(112, 35);
+            this.selectFileButton.TabIndex = 0;
+            this.selectFileButton.Text = "Select File";
+            this.selectFileButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.Click += new System.EventHandler(this.selectFileButton_Click);
             // 
             // pathTextBox
             // 
-            this.pathTextBox.Location = new System.Drawing.Point(94, 15);
+            this.pathTextBox.Location = new System.Drawing.Point(18, 65);
+            this.pathTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pathTextBox.Name = "pathTextBox";
-            this.pathTextBox.Size = new System.Drawing.Size(694, 20);
+            this.pathTextBox.Size = new System.Drawing.Size(1162, 26);
             this.pathTextBox.TabIndex = 1;
             this.pathTextBox.Text = "File/Folder...";
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(713, 42);
+            this.startButton.Location = new System.Drawing.Point(1068, 101);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.Size = new System.Drawing.Size(112, 35);
             this.startButton.TabIndex = 2;
             this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = true;
             // 
             // outputTextBox
             // 
-            this.outputTextBox.Location = new System.Drawing.Point(13, 73);
+            this.outputTextBox.Location = new System.Drawing.Point(20, 146);
+            this.outputTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.outputTextBox.Multiline = true;
             this.outputTextBox.Name = "outputTextBox";
             this.outputTextBox.ReadOnly = true;
-            this.outputTextBox.Size = new System.Drawing.Size(775, 365);
+            this.outputTextBox.Size = new System.Drawing.Size(1160, 525);
             this.outputTextBox.TabIndex = 3;
             this.outputTextBox.Text = "Output...";
             // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.AutoSize = true;
+            this.selectFolderButton.Location = new System.Drawing.Point(138, 20);
+            this.selectFolderButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(113, 35);
+            this.selectFolderButton.TabIndex = 4;
+            this.selectFolderButton.Text = "Select Folder";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.selectFolderButton_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Select folder containing .tcx files to be updated.";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.selectFolderButton);
             this.Controls.Add(this.outputTextBox);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.pathTextBox);
-            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.selectFileButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainWindow";
             this.Text = "Tcx Fixer";
             this.ResumeLayout(false);
@@ -94,10 +122,12 @@
         #endregion
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button selectFileButton;
         private System.Windows.Forms.TextBox pathTextBox;
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button selectFolderButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
--- TcxFixerGui/MainWindow.cs
+++ TcxFixerGui/MainWindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,44 @@ namespace TcxFixerGui
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void selectFileButton_Click(object sender, EventArgs e)
+        {
+            // open file dialog
+            DialogResult result = openFileDialog1.ShowDialog();            // perform file/folder action on OK click
+            if (result == DialogResult.OK)
+            {
+                string fullPath = openFileDialog1.FileName;
+                string fileName = Path.GetFileName(fullPath);
+                bool isTcxFile = Path.GetExtension(fullPath) == ".tcx" ? true : false;                // check for valid file or directory
+                if (isTcxFile)
+                {
+                    pathTextBox.Text = fileName;
+                    MessageBox.Show("File open:\n" + fullPath,
+                                    "Opened file",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Information,
+                                    MessageBoxDefaultButton.Button1);
+                }
+                else
+                {
+                    // user tried to load a dodgy file
+                    MessageBox.Show("Something went wrong when trying to open:\n" + fullPath + "\n File may not have been a vaild file type?",
+                                    "Not a tcx file",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error,
+                                    MessageBoxDefaultButton.Button1);
+                }
+            }
+        }
+
+        private void selectFolderButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathTextBox.Text = folderBrowserDialog1.SelectedPath;
+            }
         }
     }
 }
